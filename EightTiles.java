c9c0c184551c8336f2c4105cd8f7f7e6d9c4491e
Matthew Habtezgi <mtarekegn143@gmail.com>
2020-10-19 20:37:04 -0400

    Update EightTiles.java

--- EightTiles.java
+++ EightTiles.java
@@ -30,6 +30,25 @@ class TileObj{
 	}
 }
 
+class Pair{
+	TileObj[][] cur_board;
+	int amt;
+	public Pair(TileObj[][] cur_board, int amt){
+		this.cur_board = cur_board;
+		this.amt = amt;
+	}
+}
+
+class MapPair{
+	String s;
+	int amt;
+	public MapPair(String s, int amt){
+		this.amt=amt;
+		this.s=s;
+	}
+
+}
+
 
 public class EightTiles{
     private JFrame frame;
@@ -37,8 +56,8 @@ public class EightTiles{
     public static int CUR_X = -1;
     public static int CUR_Y = -1;
     public static boolean[] used = new boolean[9];
-    public static HashMap<String, Boolean> mp = new HashMap<String, Boolean>();
-    public static HashMap<TileObj,TileObj> backtrack = new HashMap<TileObj,TileObj>();//for backtracking
+    public static java.util.TreeMap<String, Boolean> mp = new TreeMap<String, Boolean>();
+    public static java.util.Map<MapPair, MapPair> backtrack = new LinkedHashMap<MapPair,MapPair>();//for backtracking
 
     public EightTiles() {
         frame = new JFrame("Bubbles Program");
@@ -54,14 +73,25 @@ public class EightTiles{
         new EightTiles();
     }
 
+    public static boolean custom_contains(MapPair trg){
+	    Set<MapPair> sm = backtrack.keySet();
+	    for(MapPair sp : sm){
+		    if(trg.s.equals(sp.s) && trg.amt==sp.amt){
+			    return true;
+		    }
+	    }
+	    return false;
+
+    }
+
     public static void swap_tiles(TileObj[][] tile, int zero_x, int zero_y, int x_p, int y_p){
+	int tmp = tile[zero_x+x_p][zero_y+y_p].vl;
+	tile[zero_x+x_p][zero_y+y_p].vl = tile[zero_x][zero_y].vl;
+	tile[zero_x][zero_y].vl=tmp;
 	tile[zero_x+x_p][zero_y+y_p].occupied=true;
 	tile[zero_x+x_p][zero_y+y_p].c = Color.BLUE;
 	tile[zero_x][zero_y].occupied=false;
 	tile[zero_x][zero_y].c=Color.RED;
-	int tmp = tile[zero_x+x_p][zero_y+y_p].vl;
-	tile[zero_x+x_p][zero_y+y_p].vl = tile[zero_x][zero_y].vl;
-	tile[zero_x][zero_y].vl=tmp;
     }
     
     public static class Tile extends JPanel implements Runnable, MouseListener  {
@@ -73,7 +103,7 @@ public class EightTiles{
         int xPos = 0;
         int yPos = 0;
         int fontSize = 20;
-       
+        public static TileObj[][] goal = new TileObj[3][3];
         Color co = new Color(255,255,255);
         Color[] coArray = {
         new Color(255,255,255), new Color(0,255,255), new Color(255,255,0),new Color(255,0,255),new Color(0,0,255)	
@@ -82,134 +112,20 @@ public class EightTiles{
 		
 	public static boolean found = false;
 	public static int n_moves = 1000000000;
-	/*
-	void pathfind(TileObj[][] tile, int num_moves, int zero_x, int zero_y){
-		
-
-		int amt = 0;
-		for(int i = 0; i < 3; i++){
-			for(int j = 0; j < 3; j++){
-				if(tile[j][i].vl==amt){
-					amt++;
-					continue;
-
-				} else {
-					break;
-				}
-			}
-		}
-		System.out.println(amt);
-		if(amt==9){
-			n_moves = Math.min(n_moves,num_moves);
-		}
-		
-		if(zero_x+1<tile[0].length){
-			swap_tiles(tile,zero_x,zero_y,1,0);
-			char[] parse = new char[9];
-			int idx = 0;
-			for(int i = 0; i < tile[0].length; i++){
-				for(int j = 0; j < tile.length; j++){
-					System.out.print(tile[i][j].vl + " ");
-					parse[tile[i][j].vl] = idx;
-					idx++;
-				}
-				System.out.println();
-			}
-			if(mp.get(String.valueOf(parse))==null){
-				mp.put(tile,true);
-				System.out.println("one");
-				backtrack.put(tile[zero_x][zero_y],tile[zero_x+1][zero_y]);
-				zero_x++;
-				pathfind(tile,num_moves+1,zero_x,zero_y);
-			}
-		}
-		if(zero_x-1>=0){
-			swap_tiles(tile,zero_x,zero_y,-1,0);
-			char[] parse = new char[9];
-			int idx = 0;
-			for(int i = 0; i < tile[0].length; i++){
-				for(int j = 0; j < tile.length; j++){
-					System.out.print(tile[i][j].vl + " ");
-					parse[tile[i][j].vl] = (char)(idx+48);
-					idx++;
-				}
-				System.out.println();
-			}
-
-			if(mp.get(String.valueOf(parse))==null){
-				mp.put(String.valueOf(parse),true);
-				System.out.println("two");
-				backtrack.put(tile[zero_x][zero_y],tile[zero_x-1][zero_y]);
-				zero_x--;
-				pathfind(tile,num_moves+1,zero_x,zero_y);
-			}
-		}
-		if(zero_y+1<tile.length){
-			swap_tiles(tile,zero_x,zero_y,0,1);
-			char[] parse = new char[9];
-			int idx = 0;
-			for(int i = 0; i < tile[0].length; i++){
-				for(int j = 0; j < tile.length; j++){
-					System.out.print(tile[i][j].vl + " ");
-					parse[tile[i][j].vl] = (char)(idx+48);
-					idx++;
-				}
-				System.out.println();
-			}
-
-			if(mp.get(String.valueOf(parse))==null){
-				mp.put(String.valueOf(parse),true);
-				System.out.println("three");
-				backtrack.put(tile[zero_x][zero_y],tile[zero_x][zero_y+1]);
-				zero_y++;
-				pathfind(tile,num_moves+1,zero_x,zero_y);
-			}
-		}
-		if(zero_y-1>=0){
-			swap_tiles(tile,zero_x,zero_y,0,-1);
-			char[] parse = new char[9];
-			int idx = 0;
-			for(int i = 0; i < tile[0].length; i++){
-				for(int j = 0; j < tile.length; j++){
-					System.out.print(tile[i][j].vl + " ");
-					parse[tile[i][j].vl] = (char)(idx+48);
-					idx++;
-				}
-				System.out.println();
-			}
-
-			if(mp.get(String.valueOf(parse))==null){
-				mp.put(String.valueOf(parse),true);
-				System.out.println("four");
-				backtrack.put(tile[zero_x][zero_y],tile[zero_x][zero_y-1]);
-				zero_y--;
-				pathfind(tile,num_moves+1,zero_x,zero_y);
-			}
-		}
-		System.out.println("ERROR");	
-	}
-    */
-
-      
-              
-        public Tile(Dimension dimension) {
-            setSize(dimension);
-            setPreferredSize(dimension);
-            addMouseListener(this);
-            addKeyListener(new TAdapter());
-            setFocusable(true);
-            d = getSize();
-
-            //for animating the screen - you won't need to edit
-            if (animator == null) {
-                animator = new Thread(this);
-                animator.start();
-            }
-	    int rw = 100;
-	    int cl = 100;
-	    int wid = 50;
-	    for(int i = 0; i < 3; i++){
+	
+	int iter = 0;
+	//fix pathfinder(hits STE)
+	//
+	
+	//w/ BFS
+	//
+	void randomize(){
+		int rw = 100;
+		int cl = 100;
+		int wid = 50;
+		   for(int i = 0; i < 3; i++){
 		    for(int j = 0; j < 3; j++){
+			    
 			    int rand = (int)(Math.random() * 9);
 			    while(used[rand]){
 				    rand=(int)(Math.random()*9);
@@ -227,18 +143,217 @@ public class EightTiles{
 		    rw+=wid+10;
 		    cl=100;
 	    }
-	    /*
+
 	    TileObj[][] cp = new TileObj[3][3];
 	    for(int i = 0; i < 3; i++){
 		    for(int j = 0; j < 3; j++){
-			    cp[i][j] = new TileObj(tile[j][i].c,tile[j][i].x,tile[j][i].y,tile[j][i].vl,tile[j][i].occupied);
+			    cp[i][j] = new TileObj(tile[i][j].c,tile[i][j].x,tile[i][j].y,tile[i][j].vl,tile[i][j].occupied);
 		    }
 	    }
-	    //pathfind(cp,0,CUR_Y,CUR_X);
-	    System.out.println(n_moves);
-	    */
-            setDoubleBuffered(true);
 
+
+	    pathfind_two(cp);
+	    Set<MapPair> sm = backtrack.keySet();
+	    backtrack(new MapPair("012345678",n_moves),sm);
+	    if(custom_contains(new MapPair("012345678",n_moves))){
+		    System.out.println("KEY EXISTS!");
+		    System.out.println(backtrack.get(new MapPair("012345678",n_moves)));
+	    } else {
+		    System.out.println("KEY DOES NOT EXIST!");
+	    }
+	    System.out.println(n_moves);
+            for(int i = 0; i < 9; i++){used[i]=false;}
+
+
+	
+
+
+	    
+
+	} 
+	
+	void backtrack(MapPair init, Set<MapPair> keys){
+		MapPair seed = init;
+		System.out.println("start");
+		while(true){
+			if(seed==null||!custom_contains(seed)){break;}
+			System.out.println(seed.s);
+			seed=backtrack.get(seed);
+		}
+		System.out.println("finish");
+	}
+	void pathfind_two(TileObj[][] board){
+		mp.clear();
+		backtrack.clear();
+		
+		java.util.Queue<Pair> e = new java.util.LinkedList<Pair>();
+		e.add(new Pair(board,0));
+		while(e.size() > 0){
+			Pair tp = e.poll();
+			int zero_x=0,zero_y=0;
+			String s="";
+			int amt_needed = 0;
+			for(int i = 0; i < 3; i++){
+				for(int j = 0; j < 3; j++){
+					if(tp.cur_board[i][j].vl == 0){
+						zero_x=i;
+						zero_y=j;
+					}
+					//System.out.print(tp.cur_board[j][i].vl + " ");
+					if(tp.cur_board[j][i].vl==amt_needed){
+						amt_needed++;
+					}
+					s += (char)(tp.cur_board[j][i].vl + 48);
+				}
+				//System.out.println();
+			}
+
+			if(amt_needed==9){
+				System.out.println("GOAL STATE SET! NUMBER OF MOVES: " + tp.amt);
+				for(int i = 0; i < 3; i++){
+					for(int j = 0; j < 3; j++){
+						goal[i][j] = tp.cur_board[j][i];
+					}
+				}
+				n_moves = Math.min(n_moves,tp.amt);
+			}
+			
+			System.out.println(s + " " + tp.amt);
+			//System.out.println(zero_x + " " + zero_y);
+
+			if(mp.get(s)==null){
+				mp.put(s,true);
+				if(zero_x+1<3){
+				//	System.out.println("ONE");
+					TileObj[][] nw = new TileObj[3][3];
+					for(int i = 0; i < 3; i++){
+						for(int j = 0; j < 3; j++){
+							nw[i][j]=new TileObj(tp.cur_board[i][j].c,tp.cur_board[i][j].x,tp.cur_board[i][j].y,tp.cur_board[i][j].vl,tp.cur_board[i][j].occupied);
+						}
+					}
+
+					swap_tiles(nw,zero_x,zero_y,1,0);
+					String st="";
+					for(int i = 0; i < 3; i++){
+						for(int j = 0; j < 3; j++){
+							st += (char)(nw[j][i].vl + 48);
+						}						
+					}
+					System.out.println("INSIDE: " + st + " " + (tp.amt + 1) + " " + s + " " + tp.amt );
+
+					MapPair p1 = new MapPair(st,tp.amt+1);
+					MapPair p2 = new MapPair(s,tp.amt);
+					backtrack.putIfAbsent(p1,p2);
+					e.add(new Pair(nw,tp.amt+1));
+
+				}
+
+				if(zero_y+1<3){
+					//System.out.println("TWO");
+					TileObj[][] nw = new TileObj[3][3];
+					for(int i = 0; i < 3; i++){
+						for(int j = 0; j < 3; j++){
+							nw[i][j]=new TileObj(tp.cur_board[i][j].c,tp.cur_board[i][j].x,tp.cur_board[i][j].y,tp.cur_board[i][j].vl,tp.cur_board[i][j].occupied);
+						}
+					}
+
+
+
+					//backtrack.put(new TileObj(tmp.c,tmp.x,tmp.y+1,tmp.vl,tmp.occupied),tmp);
+					swap_tiles(nw,zero_x,zero_y,0,1);
+					String st="";
+					for(int i = 0; i < 3; i++){
+						for(int j = 0; j < 3; j++){
+							st += (char)(nw[j][i].vl + 48);
+						}						
+					}
+					System.out.println("INSIDE: " + st + " " + (tp.amt + 1) + " " + s + " " + tp.amt );
+
+					MapPair p1 = new MapPair(st,tp.amt+1);
+					MapPair p2 = new MapPair(s,tp.amt);
+					backtrack.putIfAbsent(p1,p2);
+					e.add(new Pair(nw,tp.amt+1));
+
+				}
+
+				if(zero_x-1>=0){
+					//System.out.println("THREE");
+					TileObj[][] nw = new TileObj[3][3];
+					for(int i = 0; i < 3; i++){
+						for(int j = 0; j < 3; j++){
+							nw[i][j]=new TileObj(tp.cur_board[i][j].c,tp.cur_board[i][j].x,tp.cur_board[i][j].y,tp.cur_board[i][j].vl,tp.cur_board[i][j].occupied);
+						}
+					}
+					swap_tiles(nw,zero_x,zero_y,-1,0);
+					String st="";
+					for(int i = 0; i < 3; i++){
+						for(int j = 0; j < 3; j++){
+							st += (char)(nw[j][i].vl + 48);
+						}						
+					}
+					System.out.println("INSIDE: " + st + " " + (tp.amt + 1) + " " + s + " " + tp.amt );
+
+					MapPair p1 = new MapPair(st,tp.amt+1);
+					MapPair p2 = new MapPair(s,tp.amt);
+					backtrack.putIfAbsent(p1,p2);
+					//backtrack.put(new TileObj(tmp.c,tmp.x-1,tmp.y,tmp.vl,tmp.occupied),tmp);
+					e.add(new Pair(nw,tp.amt+1));
+
+				}
+				
+
+				if(zero_y-1>=0){
+					//System.out.println("FOUR");
+					TileObj[][] nw = new TileObj[3][3];
+					for(int i = 0; i < 3; i++){
+						for(int j = 0; j < 3; j++){
+							nw[i][j]=new TileObj(tp.cur_board[i][j].c,tp.cur_board[i][j].x,tp.cur_board[i][j].y,tp.cur_board[i][j].vl,tp.cur_board[i][j].occupied);
+						}
+					}
+
+					swap_tiles(nw,zero_x,zero_y,0,-1);
+					String st="";
+					for(int i = 0; i < 3; i++){
+						for(int j = 0; j < 3; j++){
+							st += (char)(nw[j][i].vl + 48);
+						}						
+					}
+					System.out.println("INSIDE: " + st + " " + (tp.amt + 1) + " " + s + " " + tp.amt );
+
+					MapPair p1 = new MapPair(st,tp.amt+1);
+					MapPair p2 = new MapPair(s,tp.amt);
+					backtrack.putIfAbsent(p1,p2);
+					//backtrack.put(new TileObj(tmp.c,tmp.x,tmp.y-1,tmp.vl,tmp.occupied),tmp);
+					e.add(new Pair(nw,tp.amt+1));
+				}
+
+			} else {
+				//System.out.println("VISITED");
+			}
+		}
+	}
+              
+        public Tile(Dimension dimension) {
+            setSize(dimension);
+            setPreferredSize(dimension);
+            addMouseListener(this);
+            addKeyListener(new TAdapter());
+            setFocusable(true);
+            d = getSize();
+
+            //for animating the screen - you won't need to edit
+            if (animator == null) {
+                animator = new Thread(this);
+                animator.start();
+            }
+	    int rw = 100;
+	    int cl = 100;
+	    int wid = 50;
+	    int mt = 0;
+	    randomize();
+	    
+	 	    //System.out.println(CUR_X + " " + CUR_Y);
+	    
         }
 
         @Override
@@ -274,6 +389,9 @@ public class EightTiles{
 		    row+=60;
 		    col=100;
 	    }
+	    g2.setColor(Color.YELLOW);
+	    g2.fillRect(300,300,50,50);
+
            
             g2.setColor(co);
            
@@ -318,6 +436,10 @@ public class EightTiles{
             yPos = e.getY();
 	    System.out.println(xPos + " " + yPos);
 	    area(xPos,yPos);
+	    if(e.getX() >= 300 && e.getX() <= 350 && e.getY() >= 300 && e.getY() <= 350){
+		randomize();
+	    }
+
         }
 
         public void mouseReleased(MouseEvent e) {
